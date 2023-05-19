13e59cc9b809343e4fbb6ef40b539edd3fbba00e
Radovan Bast <bast@users.noreply.github.com>
2023-05-19 15:35:32 +0200

    updating uikit is now more consistent; closes #737

--- update-uikit.sh
+++ update-uikit.sh
@@ -7,5 +7,12 @@ cd uikit
 git checkout main
 git pull origin main
 
+latest_tag=$(git describe --tags --abbrev=0)
+
 cd ..
 git add uikit
+
+old_version=$(grep -oP "uikit@(\d+\.\d+\.\d+)" templates/base.html | head -n 1)
+new_version="uikit@${latest_tag#v}"
+
+sed -i "s/${old_version}/${new_version}/g" templates/base.html
